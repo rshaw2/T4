4007e19b22ce909334f8f80052b4b738f7d5b559
Rahul Kumar Shaw <69197147+rshaw2@users.noreply.github.com>
2024-01-23 19:08:24 +0530

    Upload Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using t4.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddTransient<t4Context>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
