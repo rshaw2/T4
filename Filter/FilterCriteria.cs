513fde9d0e367b22e9f9e5dc7f2dd82b6a3dfa86
Rahul Kumar Shaw <69197147+rshaw2@users.noreply.github.com>
2024-01-23 19:08:12 +0530

    Upload FilterCriteria.cs

---
+++ Filter/FilterCriteria.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace t4.Filter
+{
+    public class FilterCriteria
+    {
+        public string PropertyName { get; set; }
+        public string Operator { get; set; }
+        public string Value { get; set; }
+    }
+}
